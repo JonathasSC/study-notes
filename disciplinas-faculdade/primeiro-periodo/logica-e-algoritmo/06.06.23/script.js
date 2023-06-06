d34eaf0cc78910712f6ed19f865d4d30f2d1e8b8
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-06-06 19:56:04 -0300

    update

--- disciplinas-faculdade/primeiro-periodo/logica-e-algoritmo/06.06.23/script.js
+++ disciplinas-faculdade/primeiro-periodo/logica-e-algoritmo/06.06.23/script.js
@@ -1,87 +1,47 @@
 function amountToPay (entrada,saida){
-    console.log("=============================")
-    console.log(" BEM VINDO AO ESTACIONAMENTO ")
-    console.log("-----------------------------")
+    let valor
 
     let hora_entrada = entrada[0]
     let min_entrada = entrada[1]
     let hora_saida = saida[0]
     let min_saida = saida[1]
 
-    let minutos_entrada = hora_entrada * 60 + min_entrada
-    let minutos_saida = hora_saida * 60 + min_saida
+    let min_total_entrada = hora_entrada * 60 + min_entrada
+    let min_total_saida = hora_saida * 60 + min_saida
 
-    let minutos_total = minutos_saida - minutos_entrada
+    let min_permanecidos = min_total_saida - min_total_entrada
 
-    if (minutos_total < 00){
-        let minutos_decimal = Math.abs(minutos_total) / 60 - 24
+    if (min_permanecidos < 0){
+        let min_decimal = Math.abs(min_permanecidos) / 60 - 24;
+        formatado = new Date(min_decimal * 3600 * 1000)
+        .toISOString().substring(11, 16)
 
-        formatado = new Date(Math.abs(minutos_decimal)
-        * 3600 * 1000).toISOString().substring(11, 16)
     } else {
-        let minutos_decimal = Math.abs(minutos_total) / 60
-        
-        formatado = new Date(Math.abs(minutos_decimal) 
-        * 3600 * 1000).toISOString().substring(11, 16)
+        let min_decimal = min_permanecidos / 60
+        formatado = new Date(min_decimal * 3600 * 1000)
+        .toISOString().substring(11, 16)
+
     }
 
     let hora = parseInt(formatado.substring(0,2))
     let min = parseInt(formatado.substring(3,5))
 
-    if (hora <= 02){
-        if (hora == 02 && min > 00){
-            console.log(`Você passou ${hora}h${min}m\nValor: R$ 8.50`)
-        } else {
-            console.log(`Você passou ${hora}h${min}m\nValor: R$ 8.00`)
-        }
-    }
-    if (hora > 02 && hora <= 04){
-        if (hora == 04 && min > 00){
-            console.log(`Você passou ${hora}h${min}m\nValor: R$ 10.50`)
-        } else {
-            console.log(`Você passou ${hora}h${min}m\nValor: R$ 8.50`)
-        }
+    if (hora <= 2){
+        valor = 8.0;
     }
 
-    if (hora >= 5){
-        if (hora == 05 && min == 00){
-            console.log(`Você passou ${hora}h${min}m\nValor: R$ 10.50`)
-
-        } else {
-            let tempo_extra = hora - 4
-
-            if (min > 00){
-                let valor_extra = tempo_extra * 2 + 2
-                console.log(`Você passou ${hora}h${min}m\nValor: R$ ${8.5 + valor_extra}`)
-
-            } else {
-                let valor_extra = tempo_extra * 2
-                console.log(`Você passou ${hora}h${min}m\nValor: R$ ${8.5 + valor_extra}`)
-
-            }
-        }
+    if (hora == 2 && min > 00 || hora > 3 || hora == 3 && min > 0){
+        valor = 8.5;
     }
 
-    console.log("-----------------------------")
-    console.log("  PAGUE AGORA COM O QRCODE   ")
-    console.log(`
-  █▀▀▀▀▀█ ▀▄▄▄█▀  █ █▀▀▀▀▀█
-  █ ███ █ ███▀▄█▀▀  █ ███ █
-  █ ▀▀▀ █ ▀ ▄▀█▀█▀▀ █ ▀▀▀ █
-  ▀▀▀▀▀▀▀ ▀ █ ▀ ▀▄▀ ▀▀▀▀▀▀▀
-  █▀██  ▀ ▀ █ ▀█▀ █▀ ▄▀▀▀▄▀
-  ▀█▀▄▀▄▀ ████▀█▀▄ █▀▀ ▄▄  
-  ▀ ▀█▄▀█ ▀▀ ▄██▄ ▀█▄█ ▀▀█
-  ▄▀▄▄ ▄▀▀ █▄ █ █ ▀▀▄▄█ ▀▀▄
-  ▀▀ ▀▀█ ▄█▄▀  █▀▀▀█▀█▀█
-  █▀▀▀▀▀█  █ ▄▀▀█▄█ ▀ █ ▀▀▀
-  █ ███ █ ▄ ▄█  ▀ ▀██▀██▄██
-  █ ▀▀▀ █ █ ▀▀▄▄██ ▀█ █ █▀ 
-  ▀▀▀▀▀▀▀ ▀▀   ▀▀   ▀▀▀▀▀▀▀
-`)
-    console.log("-----------------------------")
-    console.log("        VOLTE SEMPRE         ")
-    console.log("=============================")
+    if (hora > 4 || hora == 3 && min > 0){
+        tempo_excedido = hora - 4;
+        valor = 8.5 + tempo_excedido * 2;
+    }
+
+    console.log(`Você passou ${hora}h${min}m\nValor: R$ ${valor}`)
 }
 
-amountToPay([00,50],[01,30]) //8.00
+amountToPay([00,50],[01,30])
+amountToPay([12,50],[15,30])
+amountToPay([12,50],[19,30])
