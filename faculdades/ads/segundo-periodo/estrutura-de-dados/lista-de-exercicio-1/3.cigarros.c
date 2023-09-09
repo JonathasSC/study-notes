963733c17572452365640ec3cd8804515db989d5
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2023-09-09 16:37:21 -0300

    fix: corrigindo pequeno bug de tipo

--- faculdades/ads/segundo-periodo/estrutura-de-dados/lista-de-exercicio-1/3.cigarros.c
+++ faculdades/ads/segundo-periodo/estrutura-de-dados/lista-de-exercicio-1/3.cigarros.c
@@ -23,22 +23,22 @@ float ReceiveInfoFloat(char string[])
 
 int main()
 {
-    int anos;
-    int unidade_dia;
+    float anos;
+    float unidade_dia;
     float valor_carteira;
 
-    anos = ReceiveInfoInt("Digite a quantos anos voce fuma: ");
+    anos = ReceiveInfoFloat("Digite a quantos anos voce fuma: ");
     unidade_dia = ReceiveInfoInt("Digite quantos cigarros voce fuma diariamente: ");
     valor_carteira = ReceiveInfoFloat("Digite qual o valor da carteira de cigarro: ");
 
-    int dias = anos * 365.25;
-    int quant_fumado = dias * unidade_dia;
+    float dias = anos * 365.25;
+    float quant_fumado = dias * unidade_dia;
 
     float valor_unid = valor_carteira / 20;
     float valor_gasto = valor_unid * quant_fumado;
 
-    printf("Voce fuma ha %d dias.\n", dias);
-    printf("Voce ja fumou %d cigarros.\n", quant_fumado);
+    printf("Voce fuma ha %.0f dias.\n", dias);
+    printf("Voce ja fumou %.0f cigarros.\n", quant_fumado);
     printf("Voce ja gastou R$ %.2f em cigarros.\n", valor_gasto);
 
     return 0;
